818e6c1353c2e791941c3898931bc1e30622d7b3
Vitaliy Dobriyan <dobriy88@yandex.ru>
2023-10-24 22:56:07 +0300

    updated batch file for publishing: added aspectinjector broker dll removal

--- publish.bat
+++ publish.bat
@@ -5,3 +5,4 @@ set OUT_DIR=%2
 set PROJ_PATH=%cd%\src\TsaToolbox\TsaToolbox.csproj
 
 dotnet publish %PROJ_PATH% --configuration Release --framework net48 --output %OUT_DIR%\tsa-toolbox\net48 -p:VersionPrefix=%VERSION%
+del %OUT_DIR%\tsa-toolbox\net48\AspectInjector.Broker.dll
